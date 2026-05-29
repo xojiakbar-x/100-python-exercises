d3a6b2119732fce372691d3fa9912ac9bfd6e28d
xojiakbar-x <xojiakbarxolbayev56@gmail.com>
2026-05-29 21:43:51 +0500

    Fix formatting and comments in exercise-6.py

--- exercise-6.py
+++ exercise-6.py
@@ -2,15 +2,15 @@
 6.N natural soni berilgan. Shu songacha bo’lgan tub sonlarni
 chiqaruvchi programma tuzilsin.
 """
+
 # Daslab biror sonni qabul qilib olamiz.
 n = int(input("Sonni kiriting: "))
 print("Berilgan songacha bolgan tub sonlar.")
-"""
-Berilgan songacha bolgan barcha sonni tub deb olamiz 
-keyingi shartda xatolarni tekschiramiz
-"""
+
+# Berilgan songacha bolgan barcha sonni tub deb olamiz keyingi shartda xatolarni tekschiramiz
 for son in range(2, n+1):
     tub=True
+
 # Endi shart qoyamiz tub bolmaganlarni chiqarib tashlaymiz.
     for i in range(2, int(son**0.5)+1):
         if son%i==0:
