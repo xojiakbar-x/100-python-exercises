4705c984817f66344ca058c89f88cbb6544bf1ce
xojiakbar-x <xojiakbarxolbayev56@gmail.com>
2026-06-03 08:08:28 +0500

    Update exercise-013.py

--- exercise-013.py
+++ exercise-013.py
@@ -1,30 +1,19 @@
-n = int(input("N ni kiriting: "))  # nechta son kirishini olamiz
+"""
+Minmax4. N natural soni va n ta sonlar to’plami berilgan. Kiritilgan to’plamdagi eng
+kichik element va uning o’rnini aniqlovchi programma tuzilsin. Massivdan
+foydalanmang.
+"""
+
+n = int(input())
 
 for i in range(1, n + 1):
-    x = int(input()) 
+    x = int(input())
 
     if i == 1:
-        # birinchi sonni boshlang‘ich deb olamiz
-        min_val = x
-        max_val = x
+        eng_kichik = x
+        orni = 1
+    elif x < eng_kichik:
+        eng_kichik = x
+        orni = i
 
-        # pozitsiya (tartib raqami)
-        min_pos = 1
-        max_pos = 1
-
-    else:
-        # eng kichikni topish
-        if x < min_val:
-            min_val = x
-            min_pos = i  
-
-        # eng kattani topish
-        if x > max_val:
-            max_val = x
-            max_pos = i  
-         # katta sonni birinchisini emas oxirgisini olish   
-        elif x == max_val:
-            max_pos = i  
-
-print(min_val, min_pos)
-print(max_val, max_pos)
+print(eng_kichik, orni)
