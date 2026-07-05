8d4087dddb0d690f4383be71c23a391d9dd121a2
xojiakbar-x <xojiakbarxolbayev56@gmail.com>
2026-07-05 11:02:24 +0500

    Add files via upload

---
+++ exercise-059.py
@@ -0,0 +1,10 @@
+"""
+String58. Faylning to’liq nomini o’zida akslantirgan satr berilgan. Ya’ni disk nomi,
+kataloglar nomi, faylning nomi va kengaytmasi. Satrdan faylning nomini
+(kengaytmasiz) aniqlovchi programma tuzilsin
+"""
+s = input()
+
+oxiri = s.split("\\")[-1]
+fayl = oxiri.split(".")[0]
+print(fayl)
