ff1cd6622337f85838e8161c6db1b67394ff2d67
xojiakbar-x <xojiakbarxolbayev56@gmail.com>
2026-07-05 09:14:37 +0500

    Add files via upload

---
+++ exercise-058.py
@@ -0,0 +1,9 @@
+"""
+String57. Satr berilgan. Satrdagi ortiqcha probellarni olib tashlovchi programma
+tuzilsin.
+"""
+
+s = input("Sozlarni kiriting: ")
+natija = " ".join(s.split())
+print(natija)
+
