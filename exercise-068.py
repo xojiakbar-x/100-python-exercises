52775f35f381b0d26da5066036b337a70379aec1
xojiakbar-x <xojiakbarxolbayev56@gmail.com>
2026-07-08 08:15:59 +0500

    Refactor DoubleX to duplicate elements in array A
    Refactored the DoubleX procedure to duplicate elements equal to X in the array A. Updated input and output handling for the modified array.

--- exercise-068.py
+++ exercise-068.py
@@ -7,20 +7,19 @@ hosil bo’lgan massiv va uning o’lchami chop qilinsin.
 """
 
 def DoubleX(A, N, X):
+    
     i = 0
 
     while i < N:
         if A[i] == X:
             A.insert(i + 1, X)
             N += 1
-            i += 2   # yangi qo'shilgan elementni qayta tekshirmaslik uchun
+            i += 2   
         else:
             i += 1
 
     return N
 
-
-# Asosiy dastur
 N = int(input("N = "))
 A = list(map(int, input("Massiv elementlari: ").split()))
 X = int(input("X = "))
