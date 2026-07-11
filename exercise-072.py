66d6241b06af56040d0f8c4e041cdb33cb7af330
xojiakbar-x <xojiakbarxolbayev56@gmail.com>
2026-07-11 15:47:35 +0500

    Add files via upload

--- exercise-072.py
+++ exercise-072.py
@@ -1,14 +1,10 @@
 """
-.Recur2. N!!=N*(N-2)*(N-4)*… ifodani hisoblovchi haqiqiy toifadagi Fact2(N) rekursiv
-funksiyasi tuzilsin. (N>0 – butun toifadagi parameter; agar N juft son
-bo’lsa, ko’paytmadagi oxirgi ko’paytuvchi 2 ga va agar N toq son bo’lsa, u holda 1
-ga teng). Shu funksiya yordamida berilgan sonning ikkilangan faktoriali hisoblansin
+N!=1*2*…*N faktorialni hisoblovchi haqiqiy toifadagi Fact(N) rekursiv
+funksiyasi tuzilsin. (N > 0 – butun toifadagi parameter ). Shu funksiya yordamida
+berilgan sonning faktoriallari hisoblansin.
 """
-def Fact2(n):
-    if n == 1 or n == 2:
-        return n
-    return n * Fact2(n - 2)
-
-n = int(input("N = "))
-print("N!! =", Fact2(n))
+def Fact(n):
+    if n == 1:
+        return 1
+    return n * Fact(n - 1)
 
